9c81e0b41d6da956d03b3c29bf81826ee5a41dcf
LuydmilaKolesnikova <Lujdmila-Kolomna@yandex.ru>
2023-09-05 11:14:16 +0300

    change LineGraph.tsx

--- src/components/DashboardPage/LineGraph/LineGraph.tsx
+++ src/components/DashboardPage/LineGraph/LineGraph.tsx
@@ -9,15 +9,14 @@ interface Props {
 }
 
 const LineGraph: React.FC<Props> = (props) => {
-  let getY: any;
-  let getX: any;
-  let getYAxis: any;
-  let getXAxis: any;
-  let linePath: any;
-  let areaPath: any;
+  let getY: d3.ScaleLinear<number, number>;
+  let getX: d3.ScaleLinear<number, number>;
+  let getYAxis: React.LegacyRef<SVGGElement>;
+  let getXAxis: React.LegacyRef<SVGGElement>;
+  let linePath: string | null;
+  let areaPath: string | null;
 
   if (props.lineGraph) {
-    console.log(props.lineGraph);
     getY = d3
       .scaleLinear()
       .domain([
@@ -34,12 +33,13 @@ const LineGraph: React.FC<Props> = (props) => {
       ])
       .range([50, 1460]);
 
-    getYAxis = (ref: any) => {
-      const yAxis = d3.axisLeft(getY);
-      d3.select(ref).call(yAxis);
+    getYAxis = (ref: SVGGElement) => {
+      if (getY) {
+        const yAxis = d3.axisLeft(getY);
+        d3.select(ref).call(yAxis);
+      }
     };
-
-    getXAxis = (ref: any) => {
+    getXAxis = (ref: SVGGElement) => {
       const xAxis = d3.axisBottom(getX);
       d3.select(ref).call(xAxis);
     };
@@ -58,42 +58,39 @@ const LineGraph: React.FC<Props> = (props) => {
       .curve(d3.curveMonotoneX)(props.lineGraph.points);
   }
 
+  const Svg: React.FC = () => (
+    <svg width={1484} height={330}>
+      <g ref={getYAxis} transform={`translate(50)`} />
+      {<g ref={getXAxis} transform={`translate(0,${getY(0)})`} />}
+      {props.lineGraph.points.map((item: [number, number], index: number) => (
+        <g key={index}>
+          <circle cx={getX(item[1])} cy={getY(item[0])} r={4} fill="#7CB5EC" />
+          <text
+            fill="#666"
+            x={getX(item[1])}
+            y={getY(item[0]) - 10}
+            textAnchor="middle"
+          >
+            {item[0]}
+          </text>
+          <path
+            strokeWidth={3}
+            fill="none"
+            stroke="#7cb5ec"
+            d={linePath ?? undefined}
+          />
+          <path fill="#7cb5ec" d={areaPath ?? undefined} opacity={0.02} />
+        </g>
+      ))}
+    </svg>
+  );
+
   return (
     <div className={`${styles.lineGraph} ${commonStyles.block}`}>
       <h2 className={commonStyles.caption}>LINE GRAPH WITH GRADIENT</h2>
       {props.lineGraph && (
         <div className={styles.area}>
-          <svg width={1484} height={330}>
-            <g ref={getYAxis} transform={`translate(50)`} />
-            {<g ref={getXAxis} transform={`translate(0,${getY(0)})`} />}
-            {props.lineGraph.points.map(
-              (item: [number, number], index: number) => (
-                <g key={index}>
-                  <circle
-                    cx={getX(item[1])}
-                    cy={getY(item[0])}
-                    r={4}
-                    fill="#7CB5EC"
-                  />
-                  <text
-                    fill="#666"
-                    x={getX(item[1])}
-                    y={getY(item[0]) - 10}
-                    textAnchor="middle"
-                  >
-                    {item[0]}
-                  </text>
-                  <path
-                    strokeWidth={3}
-                    fill="none"
-                    stroke="#7cb5ec"
-                    d={linePath}
-                  />
-                  <path fill="#7cb5ec" d={areaPath} opacity={0.02} />
-                </g>
-              )
-            )}
-          </svg>
+          <Svg />
         </div>
       )}
     </div>
