d4db7e3f5bc40ddba177dbfc5938bc37d01f9e9e
LuydmilaKolesnikova <Lujdmila-Kolomna@yandex.ru>
2023-10-12 16:02:44 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC<Props> = (props) => {
   });
 
   return (
-    <HashRouter basename="/">
+    <HashRouter>
       <div className="app-wrapper">
         <HeaderContainer />
         <Routes>
